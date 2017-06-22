05a746d6e9b28c2df59d420616f5e349cadb0884
JanSunn <y_sun@berkeley.edu>
2017-06-21 21:54:48 -0500

    Update cache.R

--- cache.R
+++ cache.R
@@ -1,4 +1,9 @@
-
+# The first function  creates a special "matrix" vector
+#that
+#    set the value to Matrix
+#    get the value of Matrix
+#    set the value of the Inverse of Matrix
+#    get the value of the Inverse of Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   inver <- NULL
@@ -16,17 +21,22 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
+#This function firstly check if the matrix inverse has already been calculated. If so, output from cache.
+#Otherwise, it calculates the inverse of the matrix.
 
 
 cacheSolve <- function(x, ...) {
-
+  ## Return a matrix that is the inverse of 'x'
   inver <- x$getInverse()
+   ##Return inverse if it's in the cashe
   if (!is.null(inver)) {
     message("getting cached data")
     return(inver)
   }
+  ##Get the matrix
   matri <- x$get()
   inver <- solve(matri, ...)
   x$setInverse(inver)
+  ##output
   inver
 }
